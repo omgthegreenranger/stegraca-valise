dd78c8845b070cf900be380aa947f09854658092
omgthegreenranger <das.shaggy@gmail.com>
2023-05-06 13:17:06 -0400

    Portfolio details coming together. Start on bg.

--- stegraca-valise/src/components/details/Details.jsx
+++ stegraca-valise/src/components/details/Details.jsx
@@ -1,30 +1,43 @@
 import React, {useState, useEffect} from 'react';
 import {Container, Col, Row, Button, Modal} from 'react-bootstrap';
+import './details.css';
+import { IconContext } from 'react-icons';
+import { RxExternalLink, RxInfoCircled, RxGithubLogo } from 'react-icons/rx';
+import { DiGithub } from 'react-icons/di';
 
 export default function Details(props) {
     let work = props.projectData;
+    const clearProjects = () => {
+        props.setProjectData()
+        props.setPortOpen(true)
+    }
     return (
         <>
-        <Modal.Header closeButton>
-            <Modal.Title>{work.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div>
-                {work.description}
-            </div>
-            <div>
-                {work.id}
-            </div>
-            <div>{work.gitLink} {work.appLink}</div>
-            {work.techTags.map((tag) => {
+        <Container className="d-column details-view h-100">
+            <Row>
+                <Col><h3>{work.name}</h3></Col><Col className="d-flex icon-box"><IconContext.Provider value={{size: '3rem', color: 'white', className: 'link-icons'}}><RxGithubLogo /> <RxExternalLink /> <RxInfoCircled /></IconContext.Provider>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={2}>
+                    <div className="picture-stack"></div>
+                </Col>
+                <Col xs={8}>
+                    <Row>
+                        {work.description}
+                    </Row>
+                    <Row>
+                        <div><Button type="reset" onClick={clearProjects}>Clear</Button></div>
+                    </Row>
+                </Col>
+                <Col xs={2}>
+                    {work.techTags.map((tag) => {
                     return(
-                        <div>{tag}</div>
-                )})}
-        </Modal.Body>
-        <div>
-            <h3>{work.name}</h3>
-            <Button type="reset" onClick={()=>props.setProjectData()}>Clear</Button>
-        </div>
+                        <Row>{tag}</Row>
+                    )})}
+                </Col>
+            </Row>
+        </Container>
         </>
     )
 }
