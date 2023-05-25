be5166c5821a4d6c9e74d1a6fbd5301012764815
omgthegreenranger <das.shaggy@gmail.com>
2023-05-25 15:44:11 -0400

    Background designed is looking good

--- stegraca-valise/src/components/svg/logo.jsx
+++ stegraca-valise/src/components/svg/logo.jsx
@@ -25,22 +25,27 @@ export function Logo() {
             <stop offset="50%" stop-color="aliceblue" stop-opacity="1" />
             <stop offset="80%" stop-color="purple" />
         </linearGradient>
+        <linearGradient id="Gradient4" x1="0" x2="0" y1="0" y2="1">
+            <stop offset="35%" stop-color="black" />
+            <stop offset="50%" stop-color="grey" stop-opacity="1" />
+            <stop offset="80%" stop-color="black" />
+        </linearGradient>
     </defs>
         <svg id="shield"
             width="620"
             height="466"
             x="90"
             viewBox="00 00 620 466">
-                <polygon points="310 466, 0 31, 180 31, 233 0, 388 0, 440 31, 620 31" fill="url(#Gradient1)" />
-                <polygon className="shield" points="310 456, 10 35, 185 35, 235 7, 385 7, 435 35, 610 35" fill="url(#Gradient2)" />
-                <polygon points= "235 70, 195 50, 195 34, 235 12, 385 12, 425 34, 425 50, 385 70" fill="black" />
+                <polygon points="310 466, 0 31, 180 31, 233 0, 388 0, 440 31, 620 31" stroke="lightgreen" stroke-width="1px" fill="url(#Gradient1)" />
+                <polygon className="shield" points="310 456, 10 35, 185 35, 235 7, 385 7, 435 35, 610 35" stroke="darkgreen" stroke-width="1px" fill="url(#Gradient2)" />
+                <polygon points= "235 70, 195 50, 195 34, 235 12, 385 12, 425 34, 425 50, 385 70" stroke="black" stroke-width="2px" fill="url(#Gradient4)" />
                 <text x="220" y="50" className="stegra" textLength="190">STEGRACA.CA</text>
         </svg>
     <g className="name-build">
         <ellipse cx="250" cy="150" rx="250" ry="75" fill="transparent" stroke="black" stroke-width="3px" />
         <g className="text-line">
-            <text x="00" y="190" stroke="black" fill="url(#Gradient3)" textLength="63%">STEPHEN</text> 
-            <text x="320" y="275" stroke="black" fill="url(#Gradient3)" textLength="60%">CARDIE</text>
+            <text x="00" y="190" stroke="black" textLength="63%">STEPHEN</text> 
+            <text x="320" y="275" stroke="black" textLength="60%">CARDIE</text>
         </g>
     </g>
     
