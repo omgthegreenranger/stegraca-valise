d956eb79ace6ac3a120d28600ddc8b41756dba4b
omgthegreenranger <das.shaggy@gmail.com>
2024-03-12 14:19:39 -0400

    Updating with new local folder

---
+++ backup/Biography.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ProfilePic from "./images/profile-photo-small.jpg";
+import "./biography.css";
+
+
+export default function Bio(props) {
+  const { peepingTom } = props;
+  console.log(peepingTom)
+  return (
+    <>
+      {peepingTom ? 
+        <div className="bio-mobile bio-closed">
+          READ ABOUT ME
+        </div>
+      :
+        <BioText />
+  }
+    </>
+  );
+}
+
+function BioText() {
+  return (
+    <>
+        <div className="bio-block">
+          <div className="bio-text">
+          <p>STEPHEN CARDIE is a fullstack web developer as a second career, and is also a <span className="mastermind">Mastermind</span>.</p>
+          <p>
+            Spending over a decade on the client-facing, sales-enabling side of
+            tech, it became very apparent that his motivation was about creating
+            tools to make the job easier than the sales job itself.
+          </p>
+          <p>Clearly, he should be doing that instead.</p>
+          <p>
+            He is available for support work, code refactoring, or to help you
+            build that feature.
+          </p>
+          </div>
+          <div className="bio-photos">
+            <img src={ProfilePic} className="imager" alt="Taken by enfysphotography" />
+          </div>
+        </div>
+    </>
+  );
+}
---
+++ backup/Project.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import "./Project.css";
+import { Games } from "../index";
+import { Tab, Tabs } from "react-bootstrap";
+import projectDB from "../project/projects.json";
+import monkey from "./images/typing_monkey.svg";
+// import {Palette, usePalette } from "react-palette";
+import { GiSpy } from "react-icons/gi";
+import ColorDetector from "color-image-detector";
+
+export default function Project(props) {
+  const { portOpen, setPortOpen, projectData, setProjectData, setHoverWork } =
+    props;
+  const works = projectDB.projects;
+  const [mouseOver, setMouseOver] = useState({ toggle: false, id: "" });
+
+  function handleCards(techTags) {
+    setHoverWork(techTags);
+  }
+
+  const handleTransition = (event) => {};
+
+  return (
+    <>
+      <Tabs
+        defaultActiveKey="complete"
+        className="tab-block navs"
+        onClick={() => handleTransition()}
+        fill
+      >
+        <Tab eventKey="complete" title="Things I made" className="">
+          <div className="projectpanels">
+            <ProjectDisplay
+              works={works.filter(function (work) {
+                return work.status === "complete";
+              })}
+              portOpen={portOpen}
+              setPortOpen={setPortOpen}
+              handleCards={handleCards}
+              projectData={projectData}
+              setProjectData={setProjectData}
+              mouseOver={mouseOver}
+              setMouseOver={setMouseOver}
+              section="complete"
+            />
+          </div>
+        </Tab>
+        <Tab eventKey="in-progress" title="Things I am making">
+          <div className="projectpanels">
+            <ProjectDisplay
+              works={works.filter(function (work) {
+                return work.status === "in-progress";
+              })}
+              portOpen={portOpen}
+              setPortOpen={setPortOpen}
+              handleCards={handleCards}
+              projectData={projectData}
+              setProjectData={setProjectData}
+              mouseOver={mouseOver}
+              setMouseOver={setMouseOver}
+              section="in-progress"
+            />
+          </div>
+        </Tab>
+        {/* <Tab eventKey="fun-stuff" title="Things to do">
+          <div className="projectpanels">
+            <Games />
+          </div>
+        </Tab> */}
+      </Tabs>
+      <div
+        className={
+          portOpen ? "shortDesc shortDesc-shrunk" : "shortDesc shortDesc-big"
+        }
+      >
+        <div className="shortDesc-desc">{mouseOver.shortDesc}</div>
+      </div>
+    </>
+  );
+}
+
+function ProjectDisplay(props) {
+  const {
+    works,
+    portOpen,
+    setPortOpen,
+    handleCards,
+    setProjectData,
+    section,
+    mouseOver,
+    setMouseOver,
+  } = props;
+  const [selectedType, setSelectedType] = useState();
+  const [mousedOver, setMousedOver] = useState([false, 0]);
+
+  function handleProjectClick(work, e, shadowBox) {
+    setProjectData(work);
+    setSelectedType(section);
+    if (portOpen === false) {
+      setPortOpen(true);
+    }
+  }
+  async function loadColors(img) {
+    console.log(img)
+    let colordetector = new ColorDetector();
+    let pallets = await colordetector.detectColorPalete(img);
+ }
+
+  return (
+    <>
+      <div
+        className={
+          portOpen ? "project-cards cards-closed" : "project-cards cards-open"
+        }
+      >
+        {works.map((work, key) => {
+          function handleMouseOver(e) {
+            handleCards(work.techTags);
+            setMouseOver({
+              toggle: true,
+              id: work.id,
+              name: work.name,
+              shortDesc: work.shortDesc,
+            });
+          }
+          function handleMouseLeave(e) {
+            handleCards([]);
+            setMouseOver({ toggle: false });
+          }
+          const mapImg =
+            work.logo === "" ? monkey : require(`./images/${work.logo}`);
+
+          return (
+            <>
+            <button value="load imagem" onClick={() => loadColors()}>Hello</button>
+              <div
+                className={
+                  portOpen
+                    ? "project-card card-closed"
+                    : "project-card card-open"
+                }
+                key={key}
+                onMouseEnter={(e) => {
+                  handleMouseOver(e);
+                }}
+                onMouseLeave={(e) => {
+                  handleMouseLeave(e);
+                }}
+                onClick={(e) => handleProjectClick(work)}
+              >
+                {section === "in-progress" ? (
+                  portOpen ? (
+                    <></>
+                  ) : (
+                    <div className="card-name">
+                      <div>{work.name}</div>
+                    </div>
+                  )
+                ) : (
+                  <></>
+                )}
+                <>
+                  <img
+                    className="card-image"
+                    id="img"
+                    alt="Project Logo"
+                    src={mapImg}
+                    style={
+                      mouseOver.toggle && work.id === mouseOver.id
+                        ? { boxShadow: "0 0 1rem 0.7rem " }
+                        : { boxShadow: "0 0 0rem 0rem " }
+                    }
+                    onLoad={() => loadColors(mapImg)}
+                  />
+                  <canvas id="canvas"></canvas>
+                  {/* <div className="card-overlay"></div> */}
+                </>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+}
