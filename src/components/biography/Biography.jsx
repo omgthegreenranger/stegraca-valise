0c508a0d9cb1d55b52c974e2a2b59f12686a5fa0
Stephen Cardie <das.shaggy@gmail.com>
2024-04-15 11:13:20 -0400

    Mastermind integrated - working on transitions.

--- src/components/biography/Biography.jsx
+++ src/components/biography/Biography.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect, useTransition } from "react";
 import * as ReactDOM from 'react-dom';
 import ProfilePic from "./images/profile-photo-small.jpg";
 import "./biography.css";
 import { Mastermind } from "../index";
 import bio_text from './bio.json';
-import {
-  animated, useTrail, SpringValue, useSpring, useSprings, useChain, useSpringRef, useTransition
-} from "@react-spring/web";
+// import {
+//   animated, useTrail, SpringValue, useSpring, useSprings, useChain, useSpringRef, useTransition
+// } from "@react-spring/web";
+import { motion, useTime, AnimatePresence, useAnimationControls, useAnimate } from "framer-motion";
+
 
 export default function Bio({ peepingTom }) {
   const [bioPanel, setBioPanel] = useState(false);
@@ -30,41 +32,42 @@ function BioText({ setBioPanel }) {
   let bioText = bio_text["biography"]
   const [dropArray, setDropArray] = useState([]);
   const bioPlace = [];
-  
-  const bioLink = <span className="mastermind" onClick={() => launchApp(bioPlace)}>Mastermind</span>
 
   bioText.map((para, i) => {
     let bioWords = para.split(" ");
     bioWords.map((word, j) => {
       let wordId = i + "-" + j;
-      // console.log(wordId)
-
       let wordly = [wordId, word];
-      // setDropArray([...dropArray, wordId])
       dropArray.push([wordId])
-      console.log("Here")
-      // console.log(wordly);
       bioWords[j] = wordly
       return wordly;
 
     })
-    // console.log(bioWords)
 
     bioPlace[i] = bioWords
     return bioWords
   })
-  console.log(dropArray)
+  // console.log(dropArray)
+
   // console.log(bioPlace)
+  const dropRandom = dropArray.sort(() => Math.random() - 0.5)
+  const [isPending, startTransition] = useTransition();
 
   function launchApp() {
-    console.log("App Launched!")
-
-    setBioPanel(true);
-    const dropID = () => {
-      let dropTile = Math.floor(Math.random() * dropArray.length)
-      console.log(dropArray[dropTile])
+    startTransition(() => {
+      dropRandom.map((drop, i) => {
+        let delay = (i + 1) / 100;
+        console.log(delay)
+        document.getElementById(drop[0]).style.transitionDelay = delay + "s"
+        document.getElementById(drop[0]).classList.replace("bio-visible", "bio-invisible");
+      console.log("Done!")
+      console.log(isPending, "In function")
+    })
     }
-    console.log(dropID())
+    )
+    console.log("We're done!")
+    setBioPanel(true)
+    
   }
 
   return (
@@ -73,39 +76,27 @@ function BioText({ setBioPanel }) {
         <div className="bio-text">
           {bioPlace.map((para, i) => {
             // console.log(para)
-            return (<p id="i">
-              {para.map(word => {
-                if (word[1].includes(":link:")) {
-                  return <span className="mastermind" onClick={() => launchApp(bioPlace)}>Mastermind</span>
-                } else {
-                  return (
-                    <span className="bio-visible" id={word[0]}>{word[1]} </span>
+            return (
+                <p 
+                  // variants={container}
+                  // initial="show"
+                  // animate="hidden"
+                  id={i}>
+                  {para.map((word, j) => {
+                    if (word[1].includes(":link:")) {
+                      return <span className="mastermind" id={word[0]} onClick={() => launchApp(bioPlace)}>Mastermind.</span>
+                    } else {
+                      return (
+                        <span className="bio-words bio-visible" id={word[0]}>{word[1]} </span>
 
-                  )
-                }
-              }
-              )}
+                      )
+                    }
+                  }
+                  )}
 
-            </p>)
+                </p>
+            )
           })}
-          {/* <p>
-            STEPHEN CARDIE is a fullstack web developer as a second career, and
-            is also a{" "}
-            <span className="mastermind" onClick={() => launchApp()}>
-              Mastermind
-            </span>
-            .
-          </p>
-          <p>
-            Spending over a decade on the client-facing, sales-enabling side of
-            tech, it became very apparent that his motivation was about creating
-            tools to make the job easier than the sales job itself.
-          </p>
-          <p>Clearly, he should be doing that instead.</p>
-          <p>
-            He is available for support work, code refactoring, or to help you
-            build that feature.
-          </p> */}
         </div>
         <div className="bio-photos">
           <img
