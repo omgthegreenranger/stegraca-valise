cff39a6d249520b760ab290971a4d3d1efd915ef
Stephen Cardie <das.shaggy@gmail.com>
2025-03-03 14:41:16 -0500

    Animations have broken, somehow

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useAnimate, AnimatePresence, motion, stagger } from "motion/react";
+import { useAnimate, motion, stagger} from "motion/react";
 import "./menu.css";
 import { IoHomeSharp, IoBriefcase, IoPerson, IoPlay, IoChatbox, IoBook } from "react-icons/io5";
 
@@ -8,24 +8,26 @@ export default function Menu({ navTo, setPanelVis, setCurrentSel, setNavOpt, por
     const [loaded, setLoaded] = useState(false);
     const [secondScope, secondAnimate] = useAnimate();
 
+    // Animation order:
+    // On page load - menu items appear, staggered.
+    // On Portfolio option select ("portOpen") - menuWords disappears, but menuIcon remains.
+
+
     // This is set animation for all menu items
     // TODO: animate SVG logos
     // TODO: Create small menu.
-    const menuWords = document.getElementsByClassName("menu-text")
-    const menuIcon = document.getElementsByClassName("menu-icon")
-    const menuFront = document.getElementsByClassName("front-option")
-    const menuState = document.getElementsByClassName("menu-" + portOpen)
+    const menuWords = document.getElementsByClassName("menu-text-" + portOpen)
+    const menuIcon = document.getElementsByClassName("menu-icon") 
+    // const menuFront = document.getElementsByClassName("front-option")
     const staggerItems = stagger(0.15, {startDelay: 0.15})
+
 
     const menuSequence = [
         [menuIcon, {x: "-0rem", opacity: 1}, {duration: 0.25, delay: staggerItems }],
         [menuWords, {opacity: 1}, {duration: 1}],
-        // [menuFront, { gridTemplateColumns: "1fr auto" }]
     ]
 
-
     useEffect(() => {
-
         if(loaded) {
             const enterAnimation = async () => {
                 // await animate(scope, { opacity: 1 })
@@ -38,24 +40,24 @@ export default function Menu({ navTo, setPanelVis, setCurrentSel, setNavOpt, por
     }, [loaded, animate, menuSequence]
     )
 
-    const menuList = {
-        visible: {
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.1,
-                ease: "easeInOut"
-            },
-        },
-        hidden: {
-            opacity: 0,
-            transition: {
-            },
-        },
-    }
-    const menuItem = {
-        visible: { opacity: 1, x: 0 },
-        hidden: { opacity: 0, x: -100 },
-    }
+    // const menuList = {
+    //     visible: {
+    //         opacity: 1,
+    //         transition: {
+    //             staggerChildren: 0.1,
+    //             ease: "easeInOut"
+    //         },
+    //     },
+    //     hidden: {
+    //         opacity: 0,
+    //         transition: {
+    //         },
+    //     },
+    // }
+    // const menuItem = {
+    //     visible: { opacity: 1, x: 0 },
+    //     hidden: { opacity: 0, x: -100 },
+    // }
 
     return (
         <div className="front-bus-center">
@@ -69,12 +71,12 @@ export default function Menu({ navTo, setPanelVis, setCurrentSel, setNavOpt, por
                 // animate="visible"
                 // variants={menuList}
             >
-                <li id="home" className={"front-option menu-" + portOpen} ref={scope} ><div className="menu-icon"><IoHomeSharp /></div><div className="menu-text">Home</div></li>
-                <li id="about" className={"front-option menu-" + portOpen} ref={scope}><div className="menu-icon"><IoPerson /></div><div className="menu-text">About</div></li>
-                <li id="portfolio" className={"front-option menu-" + portOpen}ref={scope}><div className="menu-icon"><IoBriefcase /></div><div className="menu-text">Portfolio</div></li>
-                <li id="blog" className={"front-option menu-" + portOpen} ref={scope}><div className="menu-icon"><IoBook /></div><div className="menu-text">Blog</div></li>
-                <li id="doings" className={"front-option menu-" + portOpen} ref={scope}><div className="menu-icon"><IoPlay /></div><div className="menu-text">Demos</div></li>
-                <li id="contact" className={"front-option menu-" + portOpen} ref={scope}><div className="menu-icon"><IoChatbox /></div><div className="menu-text">Contact</div></li>
+                <li id="home" className="front-option" ref={scope} ><div className="menu-icon"><IoHomeSharp /></div><div className={"menu-text-" + portOpen}>Home</div></li>
+                <li id="about" className="front-option" ref={scope}><div className="menu-icon"><IoPerson /></div><div className={"menu-text-" + portOpen}>About</div></li>
+                <li id="portfolio" className="front-option" ref={scope}><div className="menu-icon"><IoBriefcase /></div><div className={"menu-text-" + portOpen}>Portfolio</div></li>
+                <li id="blog" className="front-option" ref={scope}><div className="menu-icon"><IoBook /></div><div className={"menu-text-" + portOpen}>Blog</div></li>
+                <li id="doings" className="front-option" ref={scope}><div className="menu-icon"><IoPlay /></div><div className={"menu-text-" + portOpen}>Demos</div></li>
+                <li id="contact" className="front-option" ref={scope}><div className="menu-icon"><IoChatbox /></div><div className={"menu-text-" + portOpen}>Contact</div></li>
             </motion.ul>
         </div>
     );
